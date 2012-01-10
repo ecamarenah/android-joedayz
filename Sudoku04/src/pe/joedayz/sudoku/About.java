7691853f75da0c2e404df8914460a57814c0d55e
Jose Diaz <jose.diaz@joedayz.pe>
2012-01-10 12:44:57 -0500

    Paso 4 del proyecto Sudoku

---
+++ Sudoku04/src/pe/joedayz/sudoku/About.java
@@ -0,0 +1,17 @@
+/***
+ * Taller de Aplicaciones Moviles con Android
+ * JoeDayz.pe
+***/
+package pe.joedayz.sudoku;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+	
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.about);
+   }
+}
---
+++ Sudoku04/src/pe/joedayz/sudoku/Prefs.java
@@ -0,0 +1,17 @@
+/***
+ * Taller de Aplicaciones Moviles con Android
+ * JoeDayz.pe
+***/
+package pe.joedayz.sudoku;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Prefs extends PreferenceActivity {
+	
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      addPreferencesFromResource(R.xml.settings);
+   }
+}
