d23786df290b1d4868fa49c4a2bb841add340b96
Jose Diaz <jose.diaz@joedayz.pe>
2012-02-11 16:10:36 -0500

    Controladora con metodos REST

--- spring-rest-servidor/src/main/java/com/mycompany/rest/domain/PersonList.java
+++ spring-rest-servidor/src/main/java/com/mycompany/rest/domain/PersonList.java
@@ -1,5 +1,19 @@
 package com.mycompany.rest.domain;
 
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="persons")
 public class PersonList {
 
+	private List<Person> data;
+
+	public List<Person> getData() {
+		return data;
+	}
+
+	public void setData(List<Person> data) {
+		this.data = data;
+	}
 }
